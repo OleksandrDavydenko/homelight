49b93ca86e7668122a785d8696152d14b21eca9c
Oleksandr <59095012+OleksandrDavydenko@users.noreply.github.com>
2026-01-27 12:51:34 +0000

    270126/4

--- config.py
+++ config.py
@@ -17,3 +17,4 @@ SHELLY_AUTH_KEY = os.getenv("SHELLY_AUTH_KEY", "YOUR_SHELLY_AUTH_KEY")
 SHELLY_BASE_URL = "https://shelly-237-eu.shelly.cloud"
 TARGET_MAC = "48f6eeb6ec00"  # Можна залишити None для автоматичного вибору
 TIMEZONE = os.getenv("TIMEZONE", "Europe/Kyiv")
+TELEGRAM_TOKEN = os.getenv("BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
