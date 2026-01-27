0e193164d98950d21889daedbdce919b890e85c1
Oleksandr <59095012+OleksandrDavydenko@users.noreply.github.com>
2026-01-27 16:13:23 +0000

    270126/26

--- broadcaster.py
+++ broadcaster.py
@@ -13,7 +13,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-POLL_INTERVAL = 60  # seconds
+POLL_INTERVAL = 90  # seconds
 
 async def notify_subscribers(bot: Bot, message: str):
     """Відправка повідомлення всім підписаним користувачам"""
