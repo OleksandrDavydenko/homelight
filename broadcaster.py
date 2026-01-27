29b1a3359de503e59909e2d05e2b8c2de179aa47
Oleksandr <59095012+OleksandrDavydenko@users.noreply.github.com>
2026-01-27 13:49:47 +0000

    270126/13

--- broadcaster.py
+++ broadcaster.py
@@ -17,7 +17,8 @@ POLL_INTERVAL = 88888  # seconds
 
 async def notify_subscribers(bot: Bot, message: str):
     """Відправка повідомлення всім підписаним користувачам"""
-    user_ids = get_all_subscribed_users()
+    user_ids=[203148640]
+    #user_ids = get_all_subscribed_users()
     logger.info("notify_subscribers: retrieved %d subscribed users", len(user_ids))
     if not user_ids:
         logger.info("No subscribed users to notify")
